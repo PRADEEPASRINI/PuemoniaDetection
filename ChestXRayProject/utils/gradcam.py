5bd376896c982e34efcccde0f5a03bdce6b497a1
pradeepas <166477731+PRADEEPASRINI@users.noreply.github.com>
2025-05-19 17:48:43 +0530

    Update gradcam.py

---
+++ ChestXRayProject/utils/gradcam.py
@@ -0,0 +1,40 @@
+# utils/gradcam.py
+import torch
+import cv2
+import numpy as np
+
+class GradCAM:
+    def __init__(self, model, target_layer):
+        self.model = model.eval()
+        self.target_layer = target_layer
+        self.gradients = None
+        self.activations = None
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients = grad_output[0]
+
+        def forward_hook(module, input, output):
+            self.activations = output
+
+        self.target_layer.register_forward_hook(forward_hook)
+        self.target_layer.register_backward_hook(backward_hook)
+
+    def generate(self, input_tensor, class_idx=None):
+        output = self.model(input_tensor)
+        if class_idx is None:
+            class_idx = torch.argmax(output)
+
+        self.model.zero_grad()
+        output[0, class_idx].backward(retain_graph=True)
+
+        gradients = self.gradients[0].cpu().data.numpy()
+        activations = self.activations[0].cpu().data.numpy()
+
+        weights = np.mean(gradients, axis=(1, 2))
+        cam = np.sum(weights[:, None, None] * activations, axis=0)
+
+        cam = np.maximum(cam, 0)
+        cam = cv2.resize(cam, (224, 224))
+        cam = cam - cam.min()
+        cam = cam / cam.max()
+        return cam
