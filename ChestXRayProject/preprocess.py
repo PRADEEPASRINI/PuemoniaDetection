5f0382f917610b37494611f4bdb32037f55e8602
pradeepas <166477731+PRADEEPASRINI@users.noreply.github.com>
2025-05-19 17:50:06 +0530

    Update preprocess.py

---
+++ ChestXRayProject/preprocess.py
@@ -0,0 +1,22 @@
+# preprocess.py
+import os
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+
+def get_dataloaders(data_dir='data/chest_xray', batch_size=32):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
+
+    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=transform)
+    val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'val'), transform=transform)
+    test_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
